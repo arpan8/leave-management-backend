56815aa5bc19bf6d15dbbad918b64308a6f19595
Arpan <arpanmajibally@gmail.com>
2021-11-20 19:07:36 +0530

    server setup done

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3001;
 const morgan = require('morgan');
+const path = require('path');
+const compression = require("compression");
+const helmet = require('helmet');
+const cors = require('cors');
 
-app.use(morgan('dev'))
+app.use(cors())
+app.use(helmet());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, "public")));
 
 
 app.listen(port, ()=>{
